e6d2bdd720d6d67365b1f7fcb02181a6f610dda6
Aarush Umap <aarush.umap@gmail.com>
2023-11-03 17:18:54 -0500

    Updated mutliple functions, need tests

--- autofocus-tools-loci/autofocus_tools.py
+++ autofocus-tools-loci/autofocus_tools.py
@@ -1,27 +1,62 @@
+"""
+Algorithms in this file are derived from Autofocusing Algorithm Selection in Computer Microscopy Yu Sun et al.
+"""
+
 import numpy as np
 import torch
 
 # Derivative Based Algorithms
 
-def threshold_absolute_gradient(image: np.ndarray | torch.Tensor, threshold: float) -> float:
+def threshold_absolute_gradient(image: np.ndarray | torch.Tensor, threshold: float=0, debug: bool=False) -> float:
+    """ Returns Brenner Gradient value
+    
+    "It sums the absolute value
+of the first derivative that is larger than a threshold θ"
+    
+    image: a 2D grayscale image with shape (H,W)
+    threshold: a value that each gradient must be greater than to be included in the sum
+    """
+    
     H, W = image.shape 
-    th_grad = 0
 
+    if (type(image) == np.ndarray) :
+        image: torch.Tensor = torch.from_numpy(image)
+   
+    # thresh = torch.nn.Threshold(threshold=threshold, value=0)
+    values_x: torch.Tensor = torch.abs(image[0:H-1, :] - image[1:, :])
+    values_y: torch.Tensor = torch.abs(image[:, 0:W-1] - image[:, 1: ])
+    if debug:
+        print(f"x: {values_x.shape}\ty: {values_y.shape}")
+    values_x[values_x < threshold] = 0
+    values_y[values_y < threshold] = 0
+    if debug:
+        print(f"x: {values_x.shape}\ty: {values_y.shape}")
 
+    result = torch.sum(values_x[:] + values_y[:]).item()
 
-    return 0
+    return result
 
 def squared_gradient(image: np.ndarray | torch.Tensor) -> float:
     return 0
 
-def brenner_gradient(image: np.ndarray | torch.Tensor) -> float:
+def brenner_gradient(image: np.ndarray | torch.Tensor, threshold: float=0) -> float:
+    """ Returns Brenner Gradient value
+    
+    "This algorithm computes the first difference between a pixel and its neighbor with a horizontal/vertical distance of 2"
+    
+    image: a 2D grayscale image with shape (H,W)
+    """
     H, W = image.shape
 
     if (type(image) == np.ndarray) :
         image: torch.Tensor = torch.from_numpy(image)
 
-    b_x = (image[0:H-2, :] - image[2:, :])**2
-    b_y = (image[:, 0:W-2] - image[:, 2: ])**2
+    b_x: torch.Tensor = (image[0:H-2, :] - image[2:, :])**2
+    b_y: torch.Tensor = (image[:, 0:W-2] - image[:, 2: ])**2
+    print(f"result_pre_sum: {b_x + b_y}")
+    b_x[b_x < threshold] = 0
+    b_y[b_y < threshold] = 0
+
     result = torch.sum(b_x[:] + b_y[:]).item()
     return result
 
--- autofocus-tools-loci/test-autofocus-tools.py
+++ autofocus-tools-loci/test-autofocus-tools.py
@@ -2,9 +2,15 @@ import autofocus_tools
 import numpy as np
 import torch
 
+def test_threshold_absolute_gradient(image: np.ndarray):
+    result = autofocus_tools.threshold_absolute_gradient(image, debug=True)
+    print(result)
+
+
 def test_brenner_gradient(image: np.ndarray):
     result = autofocus_tools.brenner_gradient(image)
     print(result)
 
 example_array = np.array([[1,2,3],[4,5,6],[7,8,9]])
 test_brenner_gradient(example_array)
+# test_threshold_absolute_gradient(example_array)
