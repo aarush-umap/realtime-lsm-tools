7481ecedb747c55818b17f6b42b40464ebbd98a5
Aarush Umap <aarush.umap@gmail.com>
2023-11-15 19:42:00 -0600

    Wrote test for autocorrelation

--- autofocus-tools-loci/test-autofocus-tools.py
+++ autofocus-tools-loci/test-autofocus-tools.py
@@ -1,6 +1,6 @@
 import autofocus_tools 
 import numpy as np
-import torch
+
 
 def test_threshold_absolute_gradient(image: np.ndarray):
     result = autofocus_tools.threshold_absolute_gradient(image, debug=False)
@@ -26,11 +26,16 @@ def test_energy_laplace(image: np.ndarray):
     result = autofocus_tools.energy_laplace(image)
     print(result)
 
+def test_autocorrelation(image: np.ndarray):
+    result = autofocus_tools.autocorrelation(image)
+    print(result)
+
 # Run tests
 example_array = np.array([[1,2,3],[4,5,6],[7,8,9]])
-test_threshold_absolute_gradient(example_array)
-test_brenner_gradient(example_array)
-test_squared_gradient(example_array)
-test_tenengrad(example_array)
-test_sum_of_modified_laplace(example_array)
-test_energy_laplace(example_array)
+# test_threshold_absolute_gradient(example_array)
+# test_brenner_gradient(example_array)
+# test_squared_gradient(example_array)
+# test_tenengrad(example_array)
+# test_sum_of_modified_laplace(example_array)
+# test_energy_laplace(example_array)
+test_autocorrelation(example_array)
