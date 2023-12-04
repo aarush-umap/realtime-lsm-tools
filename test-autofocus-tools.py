24ef1d7c5bd0506d9620fcd1665f34f883ceb543
Aarush Umap <aarush.umap@gmail.com>
2023-12-04 16:12:36 -0600

    Move test file out

--- test-autofocus-tools.py
+++ test-autofocus-tools.py
@@ -1,37 +1,45 @@
-import autofocus_tools 
+import autofocus_tools_loci.autofocus_tools as af
 import numpy as np
-
+import pytest
+from skimage import io
 
 def test_threshold_absolute_gradient(image: np.ndarray):
-    result = autofocus_tools.threshold_absolute_gradient(image, debug=False)
-    print(result)
+    if len(image.shape) == 3:
+        results = []
+    else:
+        result = af.threshold_absolute_gradient(image, debug=False)
+        print(result)
 
 def test_brenner_gradient(image: np.ndarray):
-    result = autofocus_tools.brenner_gradient(image)
+    result = af.brenner_gradient(image)
     print(result)
 
 def test_squared_gradient(image: np.ndarray):
-    result = autofocus_tools.squared_gradient(image, threshold=0)
+    result = af.squared_gradient(image, threshold=0)
     print(result)
     
 def test_tenengrad(image: np.ndarray):
-    result = autofocus_tools.tenenbaum_gradient(image)
+    result = af.tenenbaum_gradient(image)
     print(result)
     
 def test_sum_of_modified_laplace(image: np.ndarray):
-    result = autofocus_tools.sum_of_modified_laplace(image)
+    result = af.sum_of_modified_laplace(image)
     print(result)
 
 def test_energy_laplace(image: np.ndarray):
-    result = autofocus_tools.energy_laplace(image)
+    result = af.energy_laplace(image)
     print(result)
 
 def test_autocorrelation(image: np.ndarray):
-    result = autofocus_tools.autocorrelation(image)
+    result = af.autocorrelation(image)
     print(result)
 
 # Run tests
 example_array = np.array([[1,2,3],[4,5,6],[7,8,9]])
+acceptable = [i for i in range(21,27)]
+print(acceptable)
+test_image = io.imread(r'test_data\Zstack_HistopathologySlide_CAMM_1_MMStack_Pos0.ome.tif')
+print(test_image.shape)
 # test_threshold_absolute_gradient(example_array)
 # test_brenner_gradient(example_array)
 # test_squared_gradient(example_array)
